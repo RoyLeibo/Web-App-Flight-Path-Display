a4409e50d96b6c360978c4f1c85221fd06fc547d
einatheletz <einatheletz4@gmail.com>
2019-06-05 11:12:32 +0300

    comments

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -13,37 +13,52 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            /* 
+             * In this routes we get ip and port,
+             * and show the one point on the map
+             **/
             routes.MapRoute(
                 name: "displayLocation",
                 url: "display/{ip}/{port}",
                 defaults: new { controller = "Main", action = "DisplayLocation" },
                                 new { ip = @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" }
-
                 );
 
+            /*
+             * In this routes we get ip, port and freq,
+             * and show the route of the plane,
+             * according the freq we sample the simulator
+             **/
             routes.MapRoute(
                 name: "displayAnimation",
                 url: "display/{ip}/{port}/{freq}",
                 defaults: new { controller = "Main", action = "DisplayAnimation" }
                 );
-
-            //routes.MapRoute(
-            //    name:"saveData",
-            //    url: "save/{ip}/{port}/{freq}/{sec}/{fileName}",
-            //    defaults: new { controller = "Main", action = "SaveData" }
-            //    );
-
+            /*
+             * In this routes we get file name and freq'
+             * and show the route of the plane 
+             * according the points in the file.
+             * the freq is the time to simple point from the file.
+             **/
             routes.MapRoute(
                 name: "displayPath",
                 url: "display/{fileName}/{freq}",
                 defaults: new { controller = "Main", action = "DisplayPath" }
                 );
 
+            /*
+             * This is the defaults routes.
+             **/ 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
             );
+
+            /*
+             * In this routes we get ip,port,sec and file name,
+             * and show the route of the plane and write the loction point to file.
+             **/
             routes.MapRoute(
                 name: "sava",
                 url:"save/{ip}/{port}/{freq}/{sec}/{fileName}",
--- WebApplication1/Controllers/MainController.cs
+++ WebApplication1/Controllers/MainController.cs
@@ -31,6 +31,9 @@ namespace WebApplication1.Controllers
             }
         }
 
+        /*
+         * This function is the constructor
+         **/
         public MainController()
         {
             this.myModel = new MyModel();
@@ -38,33 +41,45 @@ namespace WebApplication1.Controllers
             this.myModel.ioFromFile.IoEvent += getLonAndLat;
             this.IsRunning = false;
         }
-
+         
+        /*
+         * Ths defult action of the defult route
+         */
         public ActionResult Index()
         {
             return View();
         }
 
-        public void CheckURL(String ip, int port)
-        {
-            try
-            {
-                IPAddress address = IPAddress.Parse(ip);
-                this.DisplayLocation(ip, port);
-            }
-            catch (Exception e)
-            {
-                this.DisplayPath(ip, port);
-            }
-        }
+        //public void CheckURL(String ip, int port)
+        //{
+        //    try
+        //    {
+        //        IPAddress address = IPAddress.Parse(ip);
+        //        this.DisplayLocation(ip, port);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        this.DisplayPath(ip, port);
+        //    }
+        //}
 
+        /*
+         * This function return the action of the DisplayLocation.
+         * connect to the simulator, get one point and show the point on the map
+         **/
         public ActionResult DisplayLocation(String ip, int port)
         {
-
             this.myModel.ioFromSimulator.ConnectInOtherThread(ip, port);
             this.myModel.ioFromSimulator.getPoint();
             return View();
         }
 
+        /*
+         * This function retune the action of DisplayAnimation.
+         * connect to the simulator, and read points from the simulator
+         * according the number of the freq that we get in the url.
+         * the points we show on the map the draw the route the plane
+         **/
         public ActionResult DisplayAnimation(String ip, int port, int freq)
         {
             Session["freq"] = freq;
@@ -73,6 +88,13 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        /*
+         * This function return the action of Save.
+         * connect to the simulator, and read points from the simulator
+         * according the number of the freq that we get in the url.
+         * in addtion to show the points on the map we save the points 
+         * in file.
+         **/
         public ActionResult Save(String ip, int port, int freq, int sec, String fileName)
         {
             Session["sec"] = sec;
@@ -81,9 +103,14 @@ namespace WebApplication1.Controllers
             this.myModel.ioFromSimulator.isWriteToFile = true;
             this.myModel.ioFromSimulator.ConnectInOtherThread(ip, port);
             return View();
-
         }
 
+        /*
+         * This function return the action of DisplayPath.
+         * read the point from the file and shoe the route that 
+         * they make on the map. the points show accordind the
+         * number of freq.
+         **/
         public ActionResult DisplayPath(String fileName, int freq)
         {
             Session["freq"] = freq;
@@ -91,11 +118,19 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        /*
+         * This function get the value of lon and lat from
+         * simulator and save them in the controller
+         **/
         public void getLonAndLat()
         {
             this.LonAndLat = this.myModel.ioFromSimulator.LonAndLat;
         }
 
+        /*
+         * This function called by the veiw and return Xml with the value 
+         * of lon amd lat.
+         **/
         [HttpPost]
         public String GetPoint()
         {
@@ -104,6 +139,7 @@ namespace WebApplication1.Controllers
             XmlWriterSettings settings = new XmlWriterSettings();
             XmlWriter writer = XmlWriter.Create(sb, settings);
             writer.WriteStartDocument();
+            //write the value into the xml
             writer.WriteStartElement("Point");
             writer.WriteElementString("Lon", Convert.ToString(this.LonAndLat.getX()));
             writer.WriteElementString("Lat", Convert.ToString(this.LonAndLat.getY()));
@@ -113,6 +149,11 @@ namespace WebApplication1.Controllers
             return sb.ToString();
         }
 
+        /*
+         * This functiom calles by the veiw and save all the data 
+         * that we read from the simulatot in file and close the 
+         * connection to the simulator.
+         **/
         [HttpPost]
         public void SaveData()
         {
@@ -120,6 +161,11 @@ namespace WebApplication1.Controllers
             this.myModel.CloseConnection();
         }
 
+        /*
+         * This function called by the view in case 4 
+         * when we read from file.
+         * and return one point each time. 
+         **/
         [HttpPost]
         public String GetPointFromFile()
         {
@@ -129,6 +175,7 @@ namespace WebApplication1.Controllers
                 XmlWriterSettings settings = new XmlWriterSettings();
                 XmlWriter writer = XmlWriter.Create(sb, settings);
                 writer.WriteStartDocument();
+                //write the value of the lon and lat in XML
                 writer.WriteStartElement("Point");
                 writer.WriteElementString("Lon", Convert.ToString(this.LonAndLat.getX()));
                 writer.WriteElementString("Lat", Convert.ToString(this.LonAndLat.getY()));
